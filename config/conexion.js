e8531626724983ec1b148a0122fbb639ef29bb5e
helgamartens0 <helgamartens@icloud.com>
2024-11-14 02:06:25 -0300

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -2,9 +2,10 @@ const sql = require('mysql2');
 
 let conexion = sql.createConnection({
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+
     user: process.env.DB_USERNAME,
-    password: process.env.PASSWORD
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME
 })
 
 conexion.connect(function(err) {
